f56ba39a426d9e1d9a9ef7fbe65df20b5a74692b
nievesfa <nievesfacorro@gmail.com>
2024-11-12 16:22:45 +0100

    Ej_09 Corregido hecho con doWhile.. doWhileee

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -7,17 +7,19 @@ public class Exercise09 {
 
     }
 
-    //
     // Comprueba el funcionamiento del bucle do-while, que para cada interacción muestre el siguiente mensaje:
     // Entra con valor: X, sale con valor: X+1
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
+
     public static void doWhileLoop(int num) {
         int x = 0;
-        while (x < num){
-        System.out.println("Entra con valor: " + x + ", " + "sale con valor: " + (x + 1));
+        do{
+            System.out.println("Entra con valor: " + x + ", " + "sale con valor: " +  (x + 1));
+            x++;
+        } while (x < num);{
+        System.out.println("Entra con valor: " + x + "," + "sale con valor: " + (x + 1));
         x++;
         }
-
     }
 }
